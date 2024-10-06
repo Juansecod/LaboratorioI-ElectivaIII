0f76cee6afced7ac17bd8b7683f20246b826069d
Juan Sebastian <juan.sebastianri28@gmail.com>
2024-10-06 15:12:57 +0000

    Implements models to: Usuario, Servicio, Contrato

--- Main/FacturasMed/admin.py
+++ Main/FacturasMed/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Usuario, Servicio, Contrato
 
 # Register your models here.
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ["id", "nombre", "apellido", "correo", "rol", "fecha_creacion", "fecha_modificacion"]
+    search_fields =["nombre", "apellido", "correo"]
+    list_filter = ["rol", "fecha_creacion", "fecha_modificacion"]
+    
+admin.site.register(Usuario, UsuarioAdmin)
+
+class ServicioAdmin(admin.ModelAdmin):
+    list_display = ["id", "nombre", "fecha_creacion"]
+    search_fields =["nombre"]
+    list_filter = ["fecha_creacion"]
+
+admin.site.register(Servicio, ServicioAdmin)
+
+class ContratoAdmin(admin.ModelAdmin):
+    list_display = ["id", "usuario", "servicio", "fecha_creacion"]
+    search_fields =["usuario__nombre", "usuario__apellidos","servicio__nombre"]
+    list_filter = ["fecha_creacion"]
+
+admin.site.register(Contrato, ContratoAdmin)
--- Main/FacturasMed/models.py
+++ Main/FacturasMed/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    nombre = models.CharField(max_length = 100, null = False, blank = False, verbose_name = "Nombre del Usuario")
+    apellido = models.CharField(max_length = 100, null = False, blank = False, verbose_name = "Apellido del Usuario")
+    correo = models.EmailField(verbose_name = "Correo")
+    contrasena = models.CharField(max_length = 255, blank = False, null  = False, verbose_name = "Contraseña")
+    fecha_creacion = models.DateTimeField(auto_now_add=True, verbose_name = "Fecha de Creación")
+    fecha_modificacion = models.DateTimeField(auto_now=True, verbose_name="Fecha de Modificiación")
+    ROLES = (
+        ("A", "Administrador"),
+        ("C", "Cliente"),
+    )
+    
+    rol = models.CharField(max_length = 1, choices = ROLES, default = "C")
+    
+    def __str__(self):
+        return f"Usuario: {self.nombre} {self.apellido}\nCorreo: {self.correo}\nRol: {self.rol}\nFecha Creación: {self.fecha_creacion.strftime('%Y-%m-%d %H:%M:%S')}\nFecha Modificación: {self.fecha_modificacion.strftime('%Y-%m-%d %H:%M:%S')}"
+
+class Servicio(models.Model):
+    nombre = models.CharField(max_length = 45, null = False, blank = False, unique = True, verbose_name = "Nombre del Servicio")
+    fecha_creacion = models.DateTimeField(auto_now_add=True, verbose_name = "Fecha de Creación")
+    
+    def __str__(self):
+        return f"Servicio: {self.nombre}\nFecha Creación: {self.fecha_creacion.strftime('%Y-%m-%d %H:%M:%S')}"
+
+class Contrato(models.Model):
+    fecha_creacion = models.DateTimeField(auto_now_add=True, verbose_name = "Fecha de Creación")
+    servicio = models.ForeignKey(Servicio, on_delete = models.PROTECT)
+    usuario = models.ForeignKey(Usuario, on_delete = models.PROTECT)
+    
+    def __str__(self):
+        return f"Contrato #: {id}\nUsuario: {self.usuario.nombre}\nTipo Servicio: {self.servicio.nombre}"
+
+class Factura(models.Model):
+    pass
