f112676bca167d73d0e4582234745d2191e2e6a0
Juan Sebastian <juan.sebastianri28@gmail.com>
2024-10-11 15:11:57 +0000

    Implements view bills and pay bills

--- Main/FacturasMed/urls.py
+++ Main/FacturasMed/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('iniciar_sesion', views.iniciar_sesion, name='iniciar_sesion'),
     path('cerrar_sesion', views.cerrar_sesion, name="cerrar_sesion"),
     path("contratos", views.ver_contratos, name="ver_contratos"),
-    path("ver_contratos", views.crear_contrato, name="crear_contrato")
+    path("contratos/crear", views.crear_contrato, name="crear_contrato"),
+    path("contratos/<int:id_contrato>/facturas", views.ver_facturas, name = 'ver_facturas'),
+    path("contratos/<int:id_contrato>/facturas/<int:id_factura>/pagar", views.pagar_factura, name = 'pagar_factura')
 ]
